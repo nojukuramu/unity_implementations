6db190466989c058fe257582c45ab10aff2fbeb3
nojukuramu <119827430+nojukuramu@users.noreply.github.com>
2024-05-01 13:53:24 +0800

    Create FollowPlayer.cs
    Attach this to an object and it will follow the player

---
+++ FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player; //Reference the player position
+    public float speed; //This will be 0 default to inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Use Vector2.MoveTowards to move this object to the player position. speed * deltaTime will work regardless of framerate
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+}
