bd9b55280fbdd4e282e781fdd18a1440a068a394
nojukuramu <119827430+nojukuramu@users.noreply.github.com>
2024-05-01 13:49:22 +0800

    Create Player2DController.cs
    Simple non-physics top-down view controller.

---
+++ Player2DController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2DController : MonoBehaviour
+{  
+    //ADD THIS SCRIPT TO YOUR PLAYER SPRITE
+    Vector2 move;
+    public float movementSpeed; //this will be default to 0. Change Value On Inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the values from Input Manager
+        float moveX = movementSpeed * Input.GetAxisRaw("Horizontal");
+        float moveY = movementSpeed * Input.GetAxisRaw("Vertical");
+
+        //get the X and Y positions, add moveX and moveY to x and y, then put them in new Vector2 so you can put the new value to transform.position
+        transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
+    }
+}
